28adc92b8237ed9bea43b8c7438acd04120ac444
Ean Dalin <eandalined@gmail.com>
2020-11-09 23:28:59 +0700

    user-info

--- src/app/user-management/user-mng-user-info-add/user-mng-user-info-add.component.ts
+++ src/app/user-management/user-mng-user-info-add/user-mng-user-info-add.component.ts
@@ -1,14 +1,19 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { StepperComponent } from '@progress/kendo-angular-layout';
-
+import { FileRestrictions, FileState, SelectEvent } from '@progress/kendo-angular-upload';
+import { Base64WriteImage } from '../../share/model/model/base64';
+import * as moment from 'moment';
+import { UploadService } from '../../share/service/upload.service';
+import { BTN_ROLES } from '../../share/constants/common.const';
 @Component({
   selector: 'app-user-mng-user-info-add',
   templateUrl: './user-mng-user-info-add.component.html',
   styleUrls: ['./user-mng-user-info-add.component.css']
 })
 export class UserMngUserInfoAddComponent implements OnInit {
- 
+  modal;
+  
   first_name: string;
   last_name: string;
   gender: string;
@@ -20,6 +25,20 @@ export class UserMngUserInfoAddComponent implements OnInit {
   description: string;
   address: string;
   currentGroup= 'abcd';
+
+  // file select declear
+  public imagePreviews: any[] = [];
+  public fileRestrictions: FileRestrictions = {
+      allowedExtensions: ['.jpg', '.png']
+  };
+  i = 0;
+// end file select declear
+
+  constructor(
+    private uploadService: UploadService,
+  ) {
+
+  }
   ngOnInit(): void {
     
   }
@@ -139,15 +158,105 @@ export class UserMngUserInfoAddComponent implements OnInit {
   }
 
   close() {
-
+    this.modal.close( {close: BTN_ROLES.CLOSE});
   }
 
-  FirstNameDelete() {
-      this.first_name = undefined;
-  }
-
-  
-  LastNameDelete() {
-    this.last_name = undefined;
+ Delete(value: string) {
+    switch(value) {
+        case 'first_name':
+            this.first_name = undefined;
+            break;
+        case 'last_name':
+            this.last_name = undefined;
+            break;
+        case 'email':
+            this.email = undefined;
+            break;
+        case 'contact':
+            this.contact = undefined;
+            break;
+        
     }
+ }
+
+ // file select function
+ public selectEventHandler(e: SelectEvent): void {
+    this.imagePreviews = [];
+    const that = this;
+    e.files.forEach((file) => {
+    let imagePreviews1 = [];
+
+    if (!file.validationErrors) {
+        const reader = new FileReader();
+        reader.onload = function (ev) {
+       
+        const image = {
+            src: ev.target['result']+"",
+            uid: file.uid,
+            id: file.uid +"-"+moment().format('YYYYMMDDhhmmss'),
+            name: file.name,
+            size: file.size,
+            type: file.rawFile.type,
+            extension: file.extension
+        };
+        imagePreviews1.push(image);
+        that.imagePreviews.unshift(image);
+
+        };
+        reader.readAsDataURL(file.rawFile);
+        
+    }
+    
+    });
+  }
+  public remove(fileSelect, uid: string) {
+      fileSelect.removeFileByUid(uid);
+      if(this.imagePreviews.length > 0) {
+        this.imagePreviews.forEach((element,index) =>{
+          if(element.uid === uid) {
+              console.log("call add function", element, index);
+              this.imagePreviews.splice(index, 1);
+          }
+        });
+      }
+    }
+
+    public showButton(state: FileState): boolean {
+      return (state === FileState.Selected) ? true : false;
+    }
+    
+upload(state) {
+  console.log(this.imagePreviews);
+  if(this.imagePreviews.length > 0) {
+    this.imagePreviews.forEach(element =>{
+      if(element.uid === state) {
+          let splitted = element.src.split(','); 
+          const base64WriteImage = new Base64WriteImage();          
+          console.log('splitted', splitted)
+          if(splitted[1]) { 
+            base64WriteImage.id         = element.id;
+            base64WriteImage.base64     = splitted[1];
+            base64WriteImage.file_name  = element.name;
+            base64WriteImage.file_type  = element.type;
+            base64WriteImage.file_size  = element.size;
+            base64WriteImage.file_extension = element.extension;
+            this.uploadService.upload(base64WriteImage).then(resp=>{
+              if(resp === true) {
+                this.resource_img_id = base64WriteImage.id;
+                console.log('resource_img_id', this.resource_img_id);
+                
+              }
+            });
+
+          }
+          
+         
+      } else {
+      }
+    });
+  }
+  
+}
+  // end file select function
+
 }
