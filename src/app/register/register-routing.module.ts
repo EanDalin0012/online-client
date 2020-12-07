c371c0713b196b0ba4d7d842a0636f0608b5c60e
Ean Dalin <eandalined@gmail.com>
2020-12-07 16:21:09 +0700

    pro duct

--- src/app/register/register-routing.module.ts
+++ src/app/register/register-routing.module.ts
@@ -3,13 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { RegiCateComponent } from './regi-cate/regi-cate.component';
 import { RegiVenComponent } from './regi-ven/regi-ven.component';
 import { RegiProComponent } from './regi-pro/regi-pro.component';
+import { RegiProDetailViewsComponent } from './regi-pro-detail-views/regi-pro-detail-views.component';
 
 
 const routes: Routes = [
    {path: 'category', component: RegiCateComponent},
    {path: 'vendor', component: RegiVenComponent},
    {path: 'product', component: RegiProComponent},
-   {path: 'view-product-detail', component: RegiProComponent}
+   {path: 'view-product-detail', component: RegiProDetailViewsComponent}
    
 ];
 
