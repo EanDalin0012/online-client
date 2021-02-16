08cb064f3859e987377294d714c7a67ca3ad91eb
Eanda <eandalined@gmail.com>
2021-02-17 00:04:47 +0700

    --- Auto-Git Commit ---

--- src/app/register/category-edit/category-edit.component.ts
+++ src/app/register/category-edit/category-edit.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { CategoryModel } from '../../share/model/model/category';
+import { TranslateService } from '@ngx-translate/core';
+import { ServerService } from '../../share/service/server.service';
+import { ModalService } from '../../share/service/modal.service';
+import { CategoryRequestModel } from '../../share/model/request/req-main-category';
+import { ResponseDataModel } from '../../share/model/response/res-data';
+import { ResponseStatus, BTN_ROLES } from '../../share/constants/common.const';
 
 @Component({
   selector: 'app-category-edit',
@@ -7,9 +14,68 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryEditComponent implements OnInit {
 
-  constructor() { }
+
+  modal;
+  typeList: any[] = [];
+  categoryModel: CategoryModel;
+  categoryName: string;
+  description: string;
+  id: number;
+
+  translateTxt: any;
+  constructor(
+    private translate: TranslateService,
+    private serverService: ServerService,
+    private modalService: ModalService
+  ) { }
 
   ngOnInit(): void {
+    this.translate.get('Home7100').subscribe((res) => {
+      this.translateTxt = res;
+    });
+    if (this.modal) {
+        this.categoryName = this.modal.message.name;
+        this.description   = this.modal.message.description;
+        this.id             = this.modal.message.id;
+     }
+  }
+
+  register(): void {
+    if ( this.isValid() === true) {
+      const categoryModel                   = new CategoryModel();
+      categoryModel.name               = this.categoryName;
+      categoryModel.description        = this.description;
+      categoryModel.id                 = this.id;
+      const api = '/api/category/v1/update';
+      this.serverService.HTTPPost(api, categoryModel).then(response => {
+        const responseData = response as ResponseDataModel;
+        if ( responseData && responseData.body.status === ResponseStatus.Y) {
+          this.modal.close( {close: BTN_ROLES.EDIT});
+        }
+      });
+    }
+}
+
+isValid(): boolean {
+  if (!this.categoryName || this.categoryName && this.categoryName.trim() === ''
+      || this.categoryName && this.categoryName === null) {
+        const bool = this.modalService.messageAlert(this.translateTxt.MESSAGE_ERROR.MAIN_CATEGORY_REQUEIRED);
+        return bool;
+  } else {
+    return true;
+  }
+}
+
+  close(): void {
+    this.modal.close( {close: BTN_ROLES.CLOSE});
+  }
+
+  onClickButtonMainCategoryName(): void {
+      this.categoryName = undefined;
+  }
+
+  onClickButtonDescription(): void {
+    this.description = undefined;
   }
 
 }
